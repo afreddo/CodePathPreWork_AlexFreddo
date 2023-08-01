3793357e1b532e721810fc5df86dceb7d43ab2c1
afreddo <freddoa@live.com>
2023-08-01 18:14:34 -0500

    Pre-work project complete

--- CodePathPreWork_AlexFreddo/ViewController.swift
+++ CodePathPreWork_AlexFreddo/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var randomText: UIButton!
+    @IBOutlet var randomBackground: UIButton!
+    
+    @IBOutlet var nameLabel: UILabel!
+    
+    @IBOutlet var positionLabel: UILabel!
+    
+    @IBOutlet var schoolLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        
+        if sender == randomBackground {
+            
+            let randomColor = changeColor()
+            view.backgroundColor = randomColor
+            
+        } else if sender == randomText {
+            
+            let randomColor = changeColor()
+            nameLabel.textColor = randomColor
+            positionLabel.textColor = randomColor
+            schoolLabel.textColor = randomColor
+            
+        }
+        
+        
+        func changeColor() -> UIColor {
+            let red = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            let blue = CGFloat.random(in: 0...1)
+            
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+        }
+        
+    }
+    
 }
 
